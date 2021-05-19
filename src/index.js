34317fba7f62ece717ea8b4e4fa73135af6c68ce
Wanderson Oliveira <wanderson1873@live.com>
2021-05-19 09:03:40 +0100

    Add Feature Create users

--- src/index.js
+++ src/index.js
@@ -1,21 +1,58 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
+
+/**
+ * id: Number,
+ * username: String,
+ * name: String
+ * todo: [
+ *    id: Number,
+ *    title: String,
+ *    done: Boolean,
+ *    deadline: Date,
+ *    created_at: Date 
+ * ]
+ */
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(
+    user => user.username === username
+  );
+
+  if(!user) return response.status(400).json({error: "User not exist!"});
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username } = request.body;
+
+  const userAlreadyExists = users.some(
+    (user) => user.username === username
+  );
+
+  if(userAlreadyExists) return response.status(400).json({error: "User already exists!"});
+
+  users.push({
+    name,
+    username,
+    id: uuidv4(),
+    todos: []
+  })
+
+  return response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
@@ -38,4 +75,4 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-module.exports = app;
+app.listen(3333);
